0839747977247d898007d5c1fe45483ac8ef2847
Marco Upia <markremembers@gmail.com>
2023-12-14 10:26:15 -0500

    solved but test is broken

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name: str, breed: str):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def set_name(self):
+        return self._name
+    
+    @set_name.setter
+    def set_name(self, name):
+            if len(name) < 1 or len(name) > 25:
+                print("Name must be a string between 1 and 25 characters.")
+            else:
+                self._name = name
+
+    @property
+    def set_breed(self):
+        return self._breed
+    
+    @set_breed.setter
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self._breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name: str, job: str):
+        self._name = None
+        self._job = None
+        self.set_name = name
+        self.set_job = job
+
+    @property
+    def set_name(self):
+        return self._name
+    
+    @set_name.setter
+    def set_name(self, name):
+        if len(name) < 1 or len(name) > 25:
+            print("Name must be a string between 1 and 25 characters.")
+        else:
+            self._name = name
+
+    @property
+    def set_job(self):
+        return self._job
+    
+    @set_job.setter
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self._job = job
